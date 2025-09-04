90c5789638268cf59b928b41c710788a1d26e373
Utkarsh-git-init <utmaurya2312@gmail.com>
2025-09-04 12:28:46 +0530

    changed credentials to placeholders

--- src/main/java/com/example/bankingsystem/Scene3Controller.java
+++ src/main/java/com/example/bankingsystem/Scene3Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.TextField;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Scene3Controller {
@@ -29,6 +30,14 @@ public class Scene3Controller {
         ps2.setInt(2,amt);
         ps2.executeUpdate();
     }
+    public boolean check(int amt) throws SQLException {
+        String sql="SELECT balance FROM users where user_id=?";
+        PreparedStatement ps=conn.prepareStatement(sql);
+        ps.setString(1,userid);
+        ResultSet rs=ps.executeQuery();
+        rs.next();
+        return rs.getInt("balance") - amt >= 0;
+    }
     public void deposit(ActionEvent event) throws SQLException {
         int amount=Integer.parseInt(amt.getText());
         update(amount,userid);
@@ -36,15 +45,23 @@ public class Scene3Controller {
     }
     public void withdraw(ActionEvent event) throws SQLException {
         int amount=Integer.parseInt(amt.getText());
-        update(-amount,userid);
-        comment.setText("withdrew amount "+amount+" from your account");
+        if(check(amount)){
+            update(-amount,userid);
+            comment.setText("withdrew amount "+amount+" from your account");
+        }else{
+            comment.setText("insufficient balance");
+        }
     }
     public void transfer(ActionEvent event) throws SQLException {
         String rid=transfer_receivers_userid.getText(); //rid:receivers id
         int amount=Integer.parseInt(amt.getText());
-        update(-amount,userid);
-        update(amount,rid);
-        comment.setText("transferred amount "+amount+" from your account to user "+rid);
+        if(check(amount)){
+            update(-amount,userid);
+            update(amount,rid);
+            comment.setText("transferred amount "+amount+" from your account to user "+rid);
+        }else{
+            comment.setText("insufficient balance");
+        }
     }
     public void back(ActionEvent event) throws SQLException, IOException {
         HelloController controller=new HelloController();
